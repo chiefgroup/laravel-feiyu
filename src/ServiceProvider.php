8fe8638679663f16fbd8ea974a339f9299bccfba
observer <sxpd7788@gmail.com>
2021-02-01 14:32:34 +0800

    init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+namespace ChiefGroup;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+/**
+ * Class ServiceProvider.
+ *
+ * @author overtrue <i@overtrue.me>
+ */
+class ServiceProvider extends LaravelServiceProvider
+{
+
+}
